5b316824cb1a44377302fda41846381bdea91d96
olinarr <olivieronardi@gmail.com>
2022-02-25 17:38:59 +0100

    Fixed post method

--- WebApp/static/generic.js
+++ WebApp/static/generic.js
@@ -7,12 +7,12 @@ function htmlToElement(html) {
 }
 
 // post a request object (dictionary) to a page (path). If inNewPage is true, open it in a new page
-function post(path, params, method='post', inNewPage = false) {
+function post(path, params, inNewPage = false) {
 
   // The rest of this code assumes you are not using a library.
   // It can be made less verbose if you use one.
   const form = document.createElement('form');
-  form.method = method;
+  form.method = 'post';
   form.action = path;
   if (inNewPage)
     form.target = "_blank" //open in new page
